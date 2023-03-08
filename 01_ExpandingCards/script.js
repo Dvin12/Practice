2da0fc5303ceeced49941d621877e594c73356eb
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-03-08 15:15:41 +0200

    JS CSS
    added removeActivated function changed text style

--- 01_ExpandingCards/script.js
+++ 01_ExpandingCards/script.js
@@ -1,7 +1,18 @@
 const panels = document.querySelectorAll(".panel");
 
+// Add 'activated' to the div
+
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
+    removeActivatedClass();
     panel.classList.add("activated");
   });
 });
+
+// Remove 'activated' from the div
+
+function removeActivatedClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("activated");
+  });
+}
