dde94a8244062ae153085f8638bbd5e7ae902d25
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-03-13 17:40:26 +0200

    added html and css

---
+++ 19_DragnDrop/script.js
@@ -0,0 +1,34 @@
+const fill = document.querySelector(".fill");
+const emptyContainers = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+for (const empty of emptyContainers) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invinsible"), 0);
+}
+function dragEnd() {
+  this.className = "fill";
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hover";
+}
+function dragLeave() {
+  this.className = "empty";
+}
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}
